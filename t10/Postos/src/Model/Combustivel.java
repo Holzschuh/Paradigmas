f3acbd18479e36fe33ff8fdaae01949bce987613
holzschuh <raraujo@inf.ufsm.br>
2015-12-01 22:36:09 -0200

    Colocando Interface

---
+++ t10/Postos/src/Model/Combustivel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Inutiu
+ */
+public class Combustivel {
+    private String tipo;
+    private float preco;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
